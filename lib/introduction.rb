970e9903f8fbf482d297e4c231f69cf4866ac384
Adam Spencer <aspencer0919@gmail.com>
2020-03-18 20:53:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,14 @@
 
 def introduction(name)
+  puts "What is your name?"
   name = gets
-  puts "Hi, my name is {name}."
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-
+  puts "what is your name?"
+  name = gets
+  puts "What language are you learning to program?"
+  language = gets
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
 end
